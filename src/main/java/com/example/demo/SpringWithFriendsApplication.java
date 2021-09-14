e69a9b663fadc5e1e198ee4255b164037b20e382
Godspeed567 <ap43333@gmail.com>
2021-09-14 12:28:39 +0530

    initial commit

---
+++ src/main/java/com/example/demo/SpringWithFriendsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithFriendsApplication.class, args);
+	}
+
+}
